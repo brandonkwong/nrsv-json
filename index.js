a738f66a8bd9a61522174c2c021a3b7afd4ada9f
Brandon Kwong <brandon@brandonkwong.com>
2020-11-22 00:54:16 -0800

    Fixed indent arg for 0

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { build, defaultOptions } = require('./scripts/build');
 const { argv } = yargs(hideBin(process.argv));
 
 const options = {
-  indent: argv.indent || defaultOptions.indent,
+  indent: argv.indent === undefined ? defaultOptions.indent : argv.index,
   map: argv.map || defaultOptions.map,
   dist: argv.dist || defaultOptions.dist,
   dataPath: argv.dataPath || defaultOptions.dataPath
